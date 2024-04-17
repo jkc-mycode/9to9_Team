9c6a5bff72499f403308fc6ce5ccfb4af991e548
rodlsdyd <rlslek3@gmail.com>
2024-04-17 17:14:04 +0900

    1.수정완료

--- main_script1.js
+++ main_script1.js
@@ -10,3 +10,19 @@ window.onclick = function (event) {
         event.target.style.display = "none";
     }
 };
+
+
+function btnopen() {
+
+    window.open('https://github.com/rodlsdyd');
+
+}
+
+function btnopen2() {
+
+    window.open('https://blog.naver.com/rlslek3');
+
+
+}
+
+
