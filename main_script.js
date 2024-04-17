4755424c39763fbaf1f7921b99375f124812a2fd
unknown <killiacaridos@gmail.com>
2024-04-17 15:40:27 +0900

    Modal 분리 기능 주석처리

--- main_script.js
+++ main_script.js
@@ -1,16 +1,16 @@
-const modal1= $("#exampleModal1").html (``)
+// const modal1= $("#exampleModal1").html (``)
 
-//홍성빈
-const modal2= $("#exampleModal2").html (``)
+// //홍성빈
+// const modal2= $("#exampleModal2").html (``)
 
-//엄혜원
-const modal3= $("#exampleModal3").html (``)
+// //엄혜원
+// const modal3= $("#exampleModal3").html (``)
 
-//이장현
-const modal4= $("#exampleModal4").html (``)
+// //이장현
+// const modal4= $("#exampleModal4").html (``)
 
-//박순호
-const modal5= $("#exampleModal5").html (``)
+// //박순호
+// const modal5= $("#exampleModal5").html (``)
 
 // // Modal 열기
 // function openModal(modalname) {
